ed975345bc29836ad86d95b87b3258b5a4ec087d
NickSterALPHA <elektrokolja@mail.ru>
2023-03-19 04:18:43 +0300

    ended gauss elimination

--- include/algebra/binary_matrix.hpp
+++ include/algebra/binary_matrix.hpp
@@ -79,31 +79,71 @@ void SwapRows(BinaryMatrix &Matrix,const int &i,const int &k) {
     std::swap(Matrix[i], Matrix[k]);
 }
 
-void GausElimination(BinaryMatrix &Matrix) {
+void SwapBits(boost::dynamic_bitset<> &bits, const int &i, const int &k) {
+    // i and k position from least bit
+    bool temp = (bool)bits[i];
+    bits[i] = bits[k];
+    bits[k] = temp; 
+}
+
+bool GaussElimination(BinaryMatrix &Matrix, boost::dynamic_bitset<> &syndrom) {
     int rows = Matrix.rowsSize(); // rows = n - k
     int columns = rows; // We need to change only right side from 0 to n - k column
 
-    // Transform to upper triangle from for right side
-    for (int i = 0; i < rows; i++) {
-        for (int j = rows - 1; j >= 0; j--) {
-            // Find not zero in column
-            if (!((bool)Matrix[i][j])) {
-                int index = -1;
-                for (int z = i + 1; z < rows; z++) {
-                    if ((bool)Matrix[z][j]) {
-                        index = z;
-                    }
-                }
 
-                // We need to check, if there are a non zero element in column
-                if (index == -1) { // Go to Next Column
-                    continue; 
-                } else { // Swipe Rows
-                    SwapRows(Matrix, i, index);
-                }
+    // Transform to upper triangle from for right side
+    int i = 0;
+    for (int j = columns - 1; j >= 0; j--) {
+        std::vector<int> pos; // Find ALl pos where 1 
+        for (int z = i + 1; z < rows; z++) {
+            if ((bool)Matrix[z][j]) {
+                pos.push_back(z);                
             }
         }
+
+
+        // Find zero in column
+        if (!((bool)Matrix[i][j])) {
+
+            // We need to check, if there are a non zero element in column
+            if (pos.empty()) { // Go to Next Column
+                i++;
+                continue; 
+            } else { // Swipe Rows
+                SwapRows(Matrix, i, pos.back());
+                SwapBits(syndrom, rows - 1 - i, rows - 1 - pos.back());
+                pos.pop_back();
+            } 
+        }
+
+
+        for (int k = 0; k < pos.size(); k++) {
+            Matrix[pos[k]] = Matrix[i] ^ Matrix[pos[k]];
+            syndrom[rows - 1 - pos[k]] = syndrom[rows - 1 - i] ^ syndrom[rows - 1 - pos[k]];
+        }
+        i++;
+    }
+    
+    bool det = 1;
+    for (i = 0; i < rows; i++) {
+        det = ((bool)Matrix[i][columns - 1 - i] && det);
     }
 
-    return;
+    if (!det) { // if det == 0
+        return false;
+    }
+
+    i = rows - 1; // last row
+    for (int j = 0; j < columns; j++) {
+        for (int z = i - 1; z >= 0; z--) {
+            if ((bool)Matrix[z][j]) {
+                Matrix[z] = Matrix[z] ^ Matrix[i];
+                syndrom[rows - 1 - z] = syndrom[rows - 1 -z] ^ syndrom[rows - 1 - i];
+            }
+        }
+        i--;
+    }
+
+    return true;
 }
+
--- tests/algebra/binary_matrix.cpp
+++ tests/algebra/binary_matrix.cpp
@@ -8,3 +8,61 @@ BOOST_AUTO_TEST_CASE(PermutationApplyingCheck1) {
 
     BOOST_CHECK(matrix == permutedMatrix);    
 }
+
+BOOST_AUTO_TEST_CASE(GaussEliminationCheck1) {
+
+    // check case then right can not transform to identity matrix
+    BinaryMatrix matrix({std::string("1011"), std::string("0111")});
+    boost::dynamic_bitset<> syndrom(2);
+    syndrom[1] = 1;
+    
+
+    bool CanTransform = GaussElimination(matrix, syndrom);
+
+    BOOST_CHECK(CanTransform == false);    
+}
+
+BOOST_AUTO_TEST_CASE(GaussEliminationCheck2) {
+
+    // simple case to construct identity matrix
+    BinaryMatrix Matrix({std::string("1001"), std::string("0110")});
+    boost::dynamic_bitset<> Syndrom(2);
+    Syndrom[1] = 1;
+    
+
+    bool CanTransform = GaussElimination(Matrix, Syndrom);
+
+    BOOST_CHECK(CanTransform == true);  
+
+    BinaryMatrix ChangedMatrix({std::string("0110"), std::string("1001")});
+    boost::dynamic_bitset<> ChangedSyndrom(2);
+    ChangedSyndrom[0] = 1;
+
+    BOOST_CHECK(Matrix == ChangedMatrix && Syndrom == ChangedSyndrom); 
+
+}
+
+
+BOOST_AUTO_TEST_CASE(GaussEliminationCheck3) {
+
+    // hard case to construct identity matrix
+    BinaryMatrix Matrix({std::string("011010100"), std::string("010111110"),
+                         std::string("101111001"), std::string("111110001")});
+    boost::dynamic_bitset<> Syndrom(4);
+    Syndrom[0] = 1;
+    Syndrom[2] = 1;
+    
+
+    bool CanTransform = GaussElimination(Matrix, Syndrom);
+
+    BOOST_CHECK(CanTransform == true);  
+
+    BinaryMatrix ChangedMatrix({std::string("010001000"), std::string("011010100"),
+                         std::string("011100010"), std::string("111110001")});
+    boost::dynamic_bitset<> ChangedSyndrom(4);
+    ChangedSyndrom[0] = 1;
+    ChangedSyndrom[3] = 1;
+
+    BOOST_CHECK(Matrix == ChangedMatrix && Syndrom == ChangedSyndrom); 
+
+}
