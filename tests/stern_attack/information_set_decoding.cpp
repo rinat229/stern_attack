4ffe2bf50e5affaeccc5f5626e2cc03c2ca1c906
rinat229 <rinat228qwertyuiop12345@gmail.com>
2023-03-21 14:26:20 +0300

    added medium size data

--- tests/stern_attack/information_set_decoding.cpp
+++ tests/stern_attack/information_set_decoding.cpp
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE PrimitivePermutationModule
 #define NUMBER_OF_SMALL_TEST_CASES 2
+#define NUMBER_OF_MEDIUM_TEST_CASES 1
 
 #include <algebra/binary_matrix.hpp>
 #include <stern_attack/information_set_decoding.hpp>
@@ -13,6 +14,36 @@ BOOST_AUTO_TEST_CASE(InformationSetDecodingSmall) {
 
     for(int i = 1; i <= NUMBER_OF_SMALL_TEST_CASES; ++i){
         std::string nameOfDirectory("small");
+        nameOfDirectory += std::to_string(i) + "/";
+        auto inputDataForMatrix = ReadLinesFromFile(nameOfDirectory + "check_matrix.txt");
+        auto inputDataForSyndrome = ReadLinesFromFile(nameOfDirectory + "syndrome.txt");
+        auto inputDataForErrorVector = ReadLinesFromFile(nameOfDirectory + "error_vector.txt");
+
+        BOOST_TEST(inputDataForMatrix.size() == inputDataForSyndrome.front().size());
+        BOOST_TEST(inputDataForMatrix.front().size() == inputDataForErrorVector.front().size());
+
+        BinaryMatrix checkMatrix;
+        for(auto& line: inputDataForMatrix) {
+            std::reverse(line.begin(), line.end());
+            checkMatrix.addRow(line);
+        }
+
+        std::reverse(inputDataForSyndrome.front().begin(), inputDataForSyndrome.front().end());
+        boost::dynamic_bitset<> syndrome(inputDataForSyndrome.front());
+
+        std::reverse(inputDataForErrorVector.front().begin(), inputDataForErrorVector.front().end());
+        boost::dynamic_bitset<> errorVector(inputDataForErrorVector.front());
+        boost::dynamic_bitset<> errorVectorFromISD = InformationSetDecoding(checkMatrix, syndrome, omega);
+
+        BOOST_TEST(errorVector == errorVectorFromISD);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(InformationSetDecodingMedium) {
+    unsigned omega = 2;
+
+    for(int i = 1; i <= NUMBER_OF_MEDIUM_TEST_CASES; ++i){
+        std::string nameOfDirectory("medium");
         nameOfDirectory += std::to_string(i) + "/";
         auto inputDataForMatrix = ReadLinesFromFile(nameOfDirectory + "check_matrix.txt");
         auto inputDataForSyndrome = ReadLinesFromFile(nameOfDirectory + "syndrome.txt");
