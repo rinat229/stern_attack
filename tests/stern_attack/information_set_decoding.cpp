82b077d2e7c59d52e74fb882f712993fbb9f32de
rinat229 <rinat228qwertyuiop12345@gmail.com>
2023-03-23 14:08:35 +0300

    adding test data

--- tests/stern_attack/information_set_decoding.cpp
+++ tests/stern_attack/information_set_decoding.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_MODULE InformationSetDecoding
 #define NUMBER_OF_SMALL_TEST_CASES 2
 #define NUMBER_OF_MEDIUM_TEST_CASES 4
+#define NUMBER_OF_BIG_TEST_CASES 2
 
 #include <algebra/binary_matrix.hpp>
 #include <stern_attack/information_set_decoding.hpp>
@@ -59,6 +60,43 @@ BOOST_AUTO_TEST_CASE(InformationSetDecodingMedium) {
         unsigned omega = numberOfErrors[i];
 
         std::string nameOfDirectory("medium");
+        nameOfDirectory += std::to_string(i) + "/";
+        auto inputDataForMatrix = ReadLinesFromFile(nameOfDirectory + "check_matrix.txt");
+        auto inputDataForSyndrome = ReadLinesFromFile(nameOfDirectory + "syndrome.txt");
+        auto inputDataForErrorVector = ReadLinesFromFile(nameOfDirectory + "error_vector.txt");
+
+        BOOST_TEST(inputDataForMatrix.size() == inputDataForSyndrome.front().size());
+        BOOST_TEST(inputDataForMatrix.front().size() == inputDataForErrorVector.front().size());
+
+        BinaryMatrix checkMatrix;
+        for(auto& line: inputDataForMatrix) {
+            std::reverse(line.begin(), line.end());
+            checkMatrix.addRow(line);
+        }
+
+        std::reverse(inputDataForSyndrome.front().begin(), inputDataForSyndrome.front().end());
+        boost::dynamic_bitset<> syndrome(inputDataForSyndrome.front());
+
+        std::reverse(inputDataForErrorVector.front().begin(), inputDataForErrorVector.front().end());
+        boost::dynamic_bitset<> errorVector(inputDataForErrorVector.front());
+        boost::dynamic_bitset<> errorVectorFromISD = InformationSetDecoding(checkMatrix, syndrome, omega);
+
+        BOOST_TEST(errorVector == errorVectorFromISD);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(InformationSetDecodingBig) {
+    std::map<int, int> numberOfErrors = {
+        {1, 12},
+        {2, 15},
+    };
+
+    for(int i = 1; i <= NUMBER_OF_BIG_TEST_CASES; ++i){
+        BOOST_CHECK(numberOfErrors.find(i) != numberOfErrors.end());
+
+        unsigned omega = numberOfErrors[i];
+
+        std::string nameOfDirectory("big");
         nameOfDirectory += std::to_string(i) + "/";
         auto inputDataForMatrix = ReadLinesFromFile(nameOfDirectory + "check_matrix.txt");
         auto inputDataForSyndrome = ReadLinesFromFile(nameOfDirectory + "syndrome.txt");
