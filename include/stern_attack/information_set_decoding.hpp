5fac4d1910e7e7fbc99fe15a450658f557e6460b
NickSterALPHA <elektrokolja@mail.ru>
2023-03-25 05:49:58 +0300

    start parallel computation

--- include/stern_attack/information_set_decoding.hpp
+++ include/stern_attack/information_set_decoding.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <boost/dynamic_bitset.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
 #include <iostream>
 #include <random>
 #include <algorithm>
@@ -10,6 +12,8 @@
 #include <permutations/combination_iterator.hpp>
 #include <permutations/primitive_permutation_iterator.hpp>
 #include <permutations/random_permutation_iterator.hpp>
+
+#include <omp.h>
 
 
 /**
@@ -68,6 +72,7 @@ boost::dynamic_bitset<> InformationSetDecoding(BinaryMatrix& checkMatrix, boost:
     boost::dynamic_bitset<> errorVector(cols);
 
     for(auto permutationIter = RandomPermutation(cols).begin(); permutationIter.CanBePermuted(); ++permutationIter) {
+        
         BinaryMatrix permutedCheckMatrix = checkMatrix.applyPermutation(*permutationIter);
         auto [isFinded, permutedErrorVector] = Step(permutedCheckMatrix, syndrome, p, omega);
 
@@ -80,6 +85,62 @@ boost::dynamic_bitset<> InformationSetDecoding(BinaryMatrix& checkMatrix, boost:
             break;
         }
     }
+
+    return errorVector;
+}
+
+
+void Thread_Task(RandomPermutation::RandomPermutationIterator& permIter, bool& IsFinded, BinaryMatrix& checkMatrix, boost::dynamic_bitset<>& syndrome,const unsigned omega,
+                 const unsigned p, boost::dynamic_bitset<>& errorVector, boost::mutex &mx ) {
+    boost::dynamic_bitset<> permutedErrorVector;
+    mx.lock();
+    while  ((!IsFinded) && (permIter.CanBePermuted())) {
+        auto my_iter = permIter;
+        permIter++;
+        mx.unlock();
+        BinaryMatrix permutedCheckMatrix = checkMatrix.applyPermutation(*my_iter);
+        std::pair<bool, boost::dynamic_bitset<> > ans = Step(permutedCheckMatrix, syndrome, p, omega);
+        IsFinded = ans.first;
+        permutedErrorVector = ans.second;
+
+        mx.lock();
+        if(ans.first) {
+            for(unsigned idx = 0; idx < my_iter->size(); ++idx){
+                errorVector[(*my_iter)[idx]] = permutedErrorVector[idx];
+            }
+            assert(checkMatrix.matVecMul(errorVector) == syndrome);
+        }
+    }
+    mx.unlock();
+    return;
+
+}
+
+
+/**
+ * Parallel algorithm of information-set decoding that find an error vector e, It works faster
+ * for check matrix H and syndrome s such that H*e = s
+ * @param checkMatrix binary permuted check matrix for which gauss elimination should be applied
+ * @param syndrome syndrome vector 
+ * @param omega number of errors in codeword (i.e. number of ones in error vector)
+ * @return error vector
+ */
+boost::dynamic_bitset<> InformationSetDecodingParallel(BinaryMatrix& checkMatrix, boost::dynamic_bitset<>& syndrome,const unsigned omega, const int &num_threads){
+    unsigned cols = checkMatrix.ColumnsSize();
+    unsigned p = static_cast<unsigned>(0.003 * cols) > 0 ? 0.003 * cols : 1;
+    boost::dynamic_bitset<> errorVector(cols);
+
+    auto permutationIter = RandomPermutation(cols).begin();
+    bool IsFinded = false;
+    boost::mutex mx;
+    boost::thread_group thr_group;
+    for (int i = 0; i < num_threads; i++) {
+        thr_group.create_thread(boost::bind(Thread_Task, boost::ref(permutationIter), boost::ref(IsFinded), boost::ref(checkMatrix),
+                                            boost::ref(syndrome), omega, p, boost::ref(errorVector), boost::ref(mx)));
+    }
+
+    thr_group.join_all();
+
 
     return errorVector;
 }
--- tests/stern_attack/information_set_decoding.cpp
+++ tests/stern_attack/information_set_decoding.cpp
@@ -5,6 +5,7 @@
 #include <algebra/binary_matrix.hpp>
 #include <stern_attack/information_set_decoding.hpp>
 #include "helper.hpp"
+#include <chrono>
 
 #include <boost/test/included/unit_test.hpp>
 #include <algorithm>
@@ -33,9 +34,19 @@ BOOST_AUTO_TEST_CASE(InformationSetDecodingSmall) {
 
         std::reverse(inputDataForErrorVector.front().begin(), inputDataForErrorVector.front().end());
         boost::dynamic_bitset<> errorVector(inputDataForErrorVector.front());
+        auto begin = std::chrono::steady_clock::now();
         boost::dynamic_bitset<> errorVectorFromISD = InformationSetDecoding(checkMatrix, syndrome, omega);
+        auto end = std::chrono::steady_clock::now();
+        auto begin_parallel = std::chrono::steady_clock::now();
+        boost::dynamic_bitset<> errorVectorFromISDParallel = InformationSetDecodingParallel(checkMatrix, syndrome, omega, 4);
+        auto end_parallel = std::chrono::steady_clock::now();
+
+        std::cout << "Time default algo - " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
+        std::cout << "Time parallel algo - " << std::chrono::duration_cast<std::chrono::milliseconds>(end_parallel - begin_parallel).count() << std::endl;
+        
 
         BOOST_TEST(errorVector == errorVectorFromISD);
+        BOOST_TEST(errorVector == errorVectorFromISDParallel);
     }
 }
 
