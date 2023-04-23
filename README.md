adc341d23ac4212f5b162358b5ef2a7b1a1ef937
rinat229 <rinat228qwertyuiop12345@gmail.com>
2023-04-23 15:59:09 +0300

    refactoring

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # stern_attack
+
+For speed, it's needed to compile with optimizations flag:
+```bash
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+```
--- include/stern_attack/base_decoding.hpp
+++ include/stern_attack/base_decoding.hpp
@@ -16,11 +16,11 @@
  * @return projected bitset
 */
 boost::dynamic_bitset<> Projection(const boost::dynamic_bitset<>& bitset_, unsigned l) {
-    boost::dynamic_bitset<> projectedBitset(l);
-    
-    for(boost::dynamic_bitset<>::size_type idx = 0; idx < l; ++idx) {
-        projectedBitset[idx] = bitset_[idx];
-    }
+    boost::dynamic_bitset<> projectedBitset = bitset_;
+    projectedBitset.resize(l);
+    // for(boost::dynamic_bitset<>::size_type idx = 0; idx < l; ++idx) {
+    //     projectedBitset[idx] = bitset_[idx];
+    // }
 
     return projectedBitset;
 }
@@ -32,23 +32,20 @@ boost::dynamic_bitset<> Projection(const boost::dynamic_bitset<>& bitset_, unsig
  * @param checkMatrix binary permuted check matrix for which gauss elimination should be applied
  * @param syndrome syndrome vector 
  * @param omega number of errors in codeword (i.e. number of ones in error vector)
- * @param step function strategy that should be applied (isd, stern, fs-isd)
+ * @param algorithm algorithm for decoding (for example, ISD, Stern)
  * @return error vector
  */
-boost::dynamic_bitset<> Decoding(BinaryMatrix& checkMatrix, boost::dynamic_bitset<>& syndrome,const unsigned omega, 
-                                std::function<std::optional<boost::dynamic_bitset<>>(
-                                    BinaryMatrix&, boost::dynamic_bitset<>, const unsigned, const unsigned, const unsigned
-                                )> step){
+template <typename DecodingStepAlgorithm>
+boost::dynamic_bitset<> Decoding(BinaryMatrix& checkMatrix, boost::dynamic_bitset<>& syndrome,
+                                 const unsigned omega, const DecodingStepAlgorithm algorithm){
     unsigned cols = checkMatrix.ColumnsSize();
-    unsigned p = static_cast<unsigned>(0.003 * cols) > 0 ? 0.003 * cols : 1;
-    unsigned l = static_cast<unsigned>(0.013 * cols) > 0 ? 0.013 * cols : 1;
 
     boost::dynamic_bitset<> errorVector(cols);
 
     for(auto permutationIter = RandomPermutation(cols).begin(); permutationIter.CanBePermuted(); ++permutationIter) {
         
         BinaryMatrix permutedCheckMatrix = checkMatrix.applyPermutation(*permutationIter);
-        auto permutedErrorVector = step(permutedCheckMatrix, syndrome, p, omega, l);
+        auto permutedErrorVector = algorithm(permutedCheckMatrix, syndrome, omega);
 
         if(permutedErrorVector) {
             for(unsigned idx = 0; idx < permutationIter->size(); ++idx){
