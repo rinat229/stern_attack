0eae270316df10f81c4a7db50aada477111e3dd3
rinat229 <rinat228qwertyuiop12345@gmail.com>
2023-04-27 02:07:31 +0300

    moving parallel algorithm to shared

--- include/stern_attack/base_decoding.hpp
+++ include/stern_attack/base_decoding.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <boost/dynamic_bitset.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+
 #include <random>
 #include <algorithm>
 #include <cassert>
@@ -85,5 +88,71 @@ boost::dynamic_bitset<> Decoding(BinaryMatrix& checkMatrix, boost::dynamic_bitse
     }
 
     std::cout << "Algorithm: " << algorithm.algorithmName << ",\tsize: " << checkMatrix.ColumnsSize() << ",\tnumber of iterations: " << numberOfIterations << '\n';
+    return errorVector;
+}
+
+
+template <typename DecodingStepAlgorithm>
+void ThreadTask(RandomPermutation::RandomPermutationIterator& permIter, BinaryMatrix& checkMatrix, 
+                boost::dynamic_bitset<>& syndrome, boost::dynamic_bitset<>& errorVector, 
+                unsigned omega, const DecodingStepAlgorithm& algorithm, boost::mutex &mx ) {
+    std::optional<boost::dynamic_bitset<>> permutedErrorVector;
+    mx.lock();
+    while  ((!permutedErrorVector) && (permIter.CanBePermuted())) {
+        auto my_iter = permIter;
+        permIter++;
+        mx.unlock();
+
+        BinaryMatrix permutedCheckMatrix = checkMatrix.applyPermutation(*my_iter);
+        auto copiedSyndrome = syndrome;
+
+        if(!algorithm.GaussElimination(permutedCheckMatrix, copiedSyndrome)){
+            continue;
+        }
+
+        auto permutedErrorVector = algorithm(permutedCheckMatrix, syndrome, omega);
+
+        mx.lock();
+        if(permutedErrorVector) {
+            for(unsigned idx = 0; idx < my_iter->size(); ++idx){
+                errorVector[(*my_iter)[idx]] = (*permutedErrorVector)[idx];
+            }
+        }
+    }
+
+    mx.unlock();
+    return;
+}
+
+
+/**
+ * Parallel algorithm of decoding that find an error vector e
+ * for check matrix H and syndrome s such that H*e = s
+ * @tparam DecodingStepAlgorithm type of algorithm (for example, ISD, Stern)
+ * @param checkMatrix binary permuted check matrix for which gauss elimination should be applied
+ * @param syndrome syndrome vector 
+ * @param omega number of errors in codeword (i.e. number of ones in error vector)
+ * @param algorithm algorithm for decoding 
+ * @param numThreads number of threads 
+ * @return error vector
+ */
+template <typename DecodingStepAlgorithm>
+boost::dynamic_bitset<> DecodingParallel(BinaryMatrix& checkMatrix, boost::dynamic_bitset<>& syndrome, 
+                                         const unsigned omega, const DecodingStepAlgorithm& algorithm,
+                                         const int &numThreads){
+    unsigned cols = checkMatrix.ColumnsSize();
+    boost::dynamic_bitset<> errorVector(cols);
+
+    auto permutationIter = RandomPermutation(cols).begin();
+    boost::mutex mx;
+    boost::thread_group thr_group;
+    for (int i = 0; i < numThreads; i++) {
+        thr_group.create_thread(boost::bind(ThreadTask<DecodingStepAlgorithm>, boost::ref(permutationIter), boost::ref(checkMatrix),
+                                boost::ref(syndrome), boost::ref(errorVector), omega,
+                                boost::ref(algorithm), boost::ref(mx)));
+    }
+
+    thr_group.join_all();
+
     return errorVector;
 }
--- include/stern_attack/information_set_decoding.hpp
+++ include/stern_attack/information_set_decoding.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <boost/dynamic_bitset.hpp>
-#include <boost/thread.hpp>
-#include <boost/thread/mutex.hpp>
 
 #include <iostream>
 #include <random>
@@ -65,57 +63,3 @@ public:
         return std::optional<boost::dynamic_bitset<>>();
     }
 };
-
-// void ThreadTask(RandomPermutation::RandomPermutationIterator& permIter, bool& IsFinded, BinaryMatrix& checkMatrix, boost::dynamic_bitset<>& syndrome,const unsigned omega,
-//                  const unsigned p, boost::dynamic_bitset<>& errorVector, boost::mutex &mx ) {
-//     boost::dynamic_bitset<> permutedErrorVector;
-//     mx.lock();
-//     while  ((!IsFinded) && (permIter.CanBePermuted())) {
-//         auto my_iter = permIter;
-//         permIter++;
-//         mx.unlock();
-//         BinaryMatrix permutedCheckMatrix = checkMatrix.applyPermutation(*my_iter);
-//         auto permutedErrorVector = InformationSetDecodingStep(permutedCheckMatrix, syndrome, p, omega);
-
-//         mx.lock();
-//         if(permutedErrorVector) {
-//             for(unsigned idx = 0; idx < my_iter->size(); ++idx){
-//                 errorVector[(*my_iter)[idx]] = (*permutedErrorVector)[idx];
-//             }
-//             assert(checkMatrix.matVecMul(errorVector) == syndrome);
-//         }
-//     }
-//     mx.unlock();
-//     return;
-
-// }
-
-
-// /**
-//  * Parallel algorithm of information-set decoding that find an error vector e, It works faster
-//  * for check matrix H and syndrome s such that H*e = s
-//  * @param checkMatrix binary permuted check matrix for which gauss elimination should be applied
-//  * @param syndrome syndrome vector 
-//  * @param omega number of errors in codeword (i.e. number of ones in error vector)
-//  * @param numThreads number of threads that can be used in algorithm
-//  * @return error vector
-//  */
-// boost::dynamic_bitset<> InformationSetDecodingParallel(BinaryMatrix& checkMatrix, boost::dynamic_bitset<>& syndrome,const unsigned omega, const int &numThreads){
-//     unsigned cols = checkMatrix.ColumnsSize();
-//     unsigned p = static_cast<unsigned>(0.003 * cols) > 0 ? 0.003 * cols : 1;
-//     boost::dynamic_bitset<> errorVector(cols);
-
-//     auto permutationIter = RandomPermutation(cols).begin();
-//     bool IsFinded = false;
-//     boost::mutex mx;
-//     boost::thread_group thr_group;
-//     for (int i = 0; i < numThreads; i++) {
-//         thr_group.create_thread(boost::bind(ThreadTask, boost::ref(permutationIter), boost::ref(IsFinded), boost::ref(checkMatrix),
-//                                             boost::ref(syndrome), omega, p, boost::ref(errorVector), boost::ref(mx)));
-//     }
-
-//     thr_group.join_all();
-
-
-//     return errorVector;
-// }
