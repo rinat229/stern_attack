4388ecdbff9d6d2bd3625deebf1cc25d41b8d6aa
rinat229 <rinat228qwertyuiop12345@gmail.com>
2023-05-07 16:12:42 +0300

    changing structure

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ For speed, it's recommend to build under release:
 ```bash
 cmake -DCMAKE_BUILD_TYPE=Release ..
 ```
+
+# **Prerequisties**
+1. Boost (dynamic_bitset, thread)
+2. libtbb-devel
+
+**Optional libraries**
+1. [nlohmann_json](https://github.com/nlohmann/json) for dumping benchmark info
---
+++ benchmark/decoding.cpp
@@ -0,0 +1,124 @@
+#include <boost/dynamic_bitset.hpp>
+#include <nlohmann/json.hpp>
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <filesystem>
+
+#include <algebra/binary_matrix.hpp>
+#include <permutations/random_permutation_iterator.hpp>
+#include <stern_attack/stern_algorithm.hpp>
+#include <stern_attack/mmt_algorithm.hpp>
+#include <utils/benchmark.hpp>
+#define assertm(exp, msg) assert(((void)msg, exp))
+ 
+
+std::vector<std::string> ReadLinesFromFile(const std::string& file) {
+    assertm(std::filesystem::exists(file), " is not found\n");
+
+    std::ifstream ifs(file);
+    std::vector<std::string> lines;
+    std::string line;
+
+    while(std::getline(ifs, line)) {
+        lines.push_back(line);
+    }
+
+    return lines;
+}
+
+
+struct DataStruct {
+    long duration;
+    long numberOfIterations;
+};
+
+
+/**
+ * @tparam DecodingStepAlgorithm type of algorithm (for example, ISD, Stern)
+ * @param checkMatrix binary permuted check matrix for which gauss elimination should be applied
+ * @param syndrome syndrome vector 
+ * @param omega number of errors in codeword (i.e. number of ones in error vector)
+ * @param algorithm algorithm for decoding 
+ * @return number of iteration and time
+ */
+template <typename DecodingStepAlgorithm>
+DataStruct DecodingBecnhmark(BinaryMatrix checkMatrix, boost::dynamic_bitset<> syndrome,
+                                 const unsigned omega, const DecodingStepAlgorithm& algorithm){
+    Timer timer(algorithm.algorithmName, 1, false);
+    unsigned cols = checkMatrix.ColumnsSize();
+
+    boost::dynamic_bitset<> errorVector(cols);
+    unsigned numberOfIterations = 0;
+    for(auto permutationIter = RandomPermutation(cols).begin(); permutationIter.CanBePermuted(); ++permutationIter, ++numberOfIterations) {
+        
+        BinaryMatrix permutedCheckMatrix = checkMatrix.applyPermutation(*permutationIter);
+        auto copiedSyndrome = syndrome;
+
+        if(!algorithm.GaussElimination(permutedCheckMatrix, copiedSyndrome)){
+            continue;
+        }
+
+        auto permutedErrorVector = algorithm(permutedCheckMatrix, copiedSyndrome, omega);
+
+        if(permutedErrorVector) {
+            for(unsigned idx = 0; idx < permutationIter->size(); ++idx){
+                errorVector[(*permutationIter)[idx]] = (*permutedErrorVector)[idx];
+            }
+
+            break;
+        }
+    }
+
+    return DataStruct{timer.GetDuration(), numberOfIterations};
+}
+
+int main(int argc, const char** argv) {
+    if(argc != 3) {
+        std::cerr << "You should provide the name of output json and number of iterations\n";
+        return 0;
+    }
+
+    std::ofstream output(argv[1]);
+    int N = std::atoi(argv[2]);
+
+    using json = nlohmann::json;
+
+    json data;
+
+    auto inputDataForMatrix = ReadLinesFromFile("benchmark/check_matrix.txt");
+    auto inputDataForSyndrome = ReadLinesFromFile("benchmark/syndrome.txt");
+    auto omega = 9;
+
+    BinaryMatrix checkMatrix;
+    for(auto& line: inputDataForMatrix) {
+        std::reverse(line.begin(), line.end());
+        checkMatrix.addRow(line);
+    }
+
+    std::reverse(inputDataForSyndrome.front().begin(), inputDataForSyndrome.front().end());
+    boost::dynamic_bitset<> syndrome(inputDataForSyndrome.front());
+
+    for(int iteration = 0; iteration < N; ++iteration) {
+        auto resultStern = DecodingBecnhmark(checkMatrix, syndrome, omega, SternAlgorithm(checkMatrix.ColumnsSize()));
+        auto resultMMT = DecodingBecnhmark(checkMatrix, syndrome, omega, MMTAlgorithm(checkMatrix.ColumnsSize(), checkMatrix.RowsSize()));
+
+        data["stern"][iteration] = {
+            {"iterations_count", resultStern.numberOfIterations},
+            {"duration", resultStern.duration},
+        };
+
+        data["MMT"][iteration] = {
+            {"iterations_count", resultMMT.numberOfIterations},
+            {"duration", resultMMT.duration},
+        };
+    }
+
+    data["params"]["n"] = checkMatrix.ColumnsSize();
+    data["params"]["k"] = checkMatrix.ColumnsSize() - checkMatrix.RowsSize();
+    data["params"]["omega"] = omega;
+    output << std::setw(4) << data << '\n';
+}
