d026a2a8f3a74e832d6bb4a14032b05a85c56052
rinat229 <rinat228qwertyuiop12345@gmail.com>
2023-05-11 16:03:57 +0300

    MMT hash: removing extra list on l2

--- include/stern_attack/hash_mmt_algorithm.hpp
+++ include/stern_attack/hash_mmt_algorithm.hpp
@@ -51,7 +51,7 @@ public:
         auto combination = Combination(p, halfColsSizeOfQ);
         auto numberOfCombs = combination.GetNumberOfCombinations();
 
-        std::vector<CollisionType> projectedSum1;
+        // std::vector<CollisionType> projectedSum1;
         std::unordered_multimap<BinaryMatrix::BitContainerType, IndexType> projectedSum2;
 
         std::vector<CollisionType> /*projectedSum11,*/ projectedSum12;
@@ -61,7 +61,7 @@ public:
         projectedSum21.reserve(expectedLengthLevel1);
         projectedSum22.reserve(expectedLengthLevel1);
 
-        projectedSum1.reserve(expectedLengthLevel2);
+        // projectedSum1.reserve(expectedLengthLevel2);
         projectedSum2.reserve(expectedLengthLevel2);
 
         boost::dynamic_bitset<> projectedSyndrome1 = Projection(syndrome, l1);
@@ -92,43 +92,6 @@ public:
         boost::sort::pdqsort_branchless(projectedSum12.begin(), projectedSum12.end(), compare);
         boost::sort::pdqsort_branchless(projectedSum21.begin(), projectedSum21.end(), compare);
         boost::sort::pdqsort_branchless(projectedSum22.begin(), projectedSum22.end(), compare);
-
-        for(auto iter1 = projectedSum12.begin(), iter2 = projectedSum12.begin(); iter1 != projectedSum12.end() && iter2 != projectedSum12.end();) {
-            if(iter1->first < iter2->first){
-                ++iter1;
-            } else if(iter1->first > iter2->first) {
-                ++iter2;
-            } else {
-                auto iterEnd1 = std::find_if(iter1, projectedSum12.end(), [&iter1] (auto &iter) {
-                    return iter.first != iter1->first;
-                });
-
-                auto iterEnd2 = std::find_if(iter2, projectedSum12.end(), [&iter2] (auto &iter) {
-                    return iter.first != iter2->first;
-                });
-
-                for(auto iterBegin1 = iter1; iterBegin1 != iterEnd1; ++iterBegin1){
-                    for(auto iterBegin2 = iter2; iterBegin2 != iterEnd2; ++iterBegin2){
-                        if(iterBegin1->first == iterBegin2->first) {
-                            std::vector<unsigned> indexUnion;
-                            indexUnion.reserve(iterBegin1->second.size() + iterBegin2->second.size());
-                            indexUnion.insert(indexUnion.end(), iterBegin1->second.begin(), iterBegin1->second.end());
-                            indexUnion.insert(indexUnion.end(), iterBegin2->second.begin(), iterBegin2->second.end());
-
-                            /// TODO: is it a need in recomputation of sum of columns???
-                            projectedSum1.emplace_back(checkMatrixOnL1.sumOfRows(indexUnion), indexUnion);
-                        }
-                    }
-                }
-
-                if(projectedSum1.size() > expectedLengthLevel2) {
-                    break;
-                }
-
-                iter1 = iterEnd1;
-                iter2 = iterEnd2;
-            }
-        }
 
         for(auto iter1 = projectedSum21.begin(), iter2 = projectedSum22.begin(); iter1 != projectedSum21.end() && iter2 != projectedSum22.end();) {
             if(iter1->first < iter2->first){
@@ -167,18 +130,7 @@ public:
             }
         }
 
-        lengthsL2.push_back(projectedSum1.size());
-        lengthsL2.push_back(projectedSum2.size());
-        // indexes of columns should guarantee to be sorted for finding symmetric difference
-        std::for_each(projectedSum1.begin(), projectedSum1.end(), [](CollisionType& element) {
-            std::sort(element.second.begin(), element.second.end());
-        });
-
-        std::for_each(projectedSum2.begin(), projectedSum2.end(), [](CollisionType element) {
-            std::sort(element.second.begin(), element.second.end());
-        });
-
-        for(auto &[projectedOnL1Bitset, indexSet1] : projectedSum1) {
+        const auto findCollision = [&](auto&& projectedOnL1Bitset, auto&& indexSet1){
             auto [iterBegin, iterEnd] = projectedSum2.equal_range(projectedOnL1Bitset);
 
             for(auto iterMatched = iterBegin; iterMatched != iterEnd; ++iterMatched){
@@ -202,12 +154,51 @@ public:
                             errorVector.set(setBitIdx + checkMatrix.ColumnsSize() - checkMatrix.RowsSize());
                         }
                     }
-                    // std::cout << checkMatrix.matVecMul(errorVector) << '\n';
-                    // std::cout << syndrome << "\n\n";
-                    // assert(checkMatrix.matVecMul(errorVector) == syndrome);
 
                     return std::optional<boost::dynamic_bitset<>>(errorVector);
                 }
+            }
+
+            return std::optional<boost::dynamic_bitset<>>();
+        };
+
+        lengthsL2.push_back(projectedSum2.size());
+
+        std::for_each(projectedSum2.begin(), projectedSum2.end(), [](CollisionType element) {
+            std::sort(element.second.begin(), element.second.end());
+        });
+
+        for(auto iter1 = projectedSum12.begin(), iter2 = projectedSum12.begin(); iter1 != projectedSum12.end() && iter2 != projectedSum12.end();) {
+            if(iter1->first < iter2->first){
+                ++iter1;
+            } else if(iter1->first > iter2->first) {
+                ++iter2;
+            } else {
+                auto iterEnd1 = std::find_if(iter1, projectedSum12.end(), [&iter1] (auto &iter) {
+                    return iter.first != iter1->first;
+                });
+
+                auto iterEnd2 = std::find_if(iter2, projectedSum12.end(), [&iter2] (auto &iter) {
+                    return iter.first != iter2->first;
+                });
+
+                for(auto iterBegin1 = iter1; iterBegin1 != iterEnd1; ++iterBegin1){
+                    for(auto iterBegin2 = iter2; iterBegin2 != iterEnd2; ++iterBegin2){
+                        if(iterBegin1->first == iterBegin2->first) {
+                            std::vector<unsigned> indexUnion;
+                            indexUnion.reserve(iterBegin1->second.size() + iterBegin2->second.size());
+                            indexUnion.insert(indexUnion.end(), iterBegin1->second.begin(), iterBegin1->second.end());
+                            indexUnion.insert(indexUnion.end(), iterBegin2->second.begin(), iterBegin2->second.end());
+
+                            if(auto findedErrorVector = findCollision(checkMatrixOnL1.sumOfRows(indexUnion), std::move(indexUnion)); findedErrorVector){
+                                return findedErrorVector;
+                            }
+                        }
+                    }
+                }
+
+                iter1 = iterEnd1;
+                iter2 = iterEnd2;
             }
         }
 
